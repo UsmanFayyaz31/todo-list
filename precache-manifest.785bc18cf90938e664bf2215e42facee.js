e6f89b2aa37c780e4095c53ec655dd230fa84437
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2019-08-07 16:04:09 +0500

    Updates

---
+++ precache-manifest.785bc18cf90938e664bf2215e42facee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6203d27ffa31441ed2c17af5f50656a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "c0ea9b5d108c22419f60",
+    "url": "/todo-list/static/css/main.534c771d.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/todo-list/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "c0ea9b5d108c22419f60",
+    "url": "/todo-list/static/js/main.992eb62c.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
